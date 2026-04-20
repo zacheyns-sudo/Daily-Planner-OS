7ea8d579e2162ab5a644d8efdda772944bdd118e
Zac Heyns <zacheyns@macbookpro.home>
2026-04-20 12:25:09 +0200

    Add Lecture / Class / PBL option to calendar event categories

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'daily-os-v15';
+const CACHE = 'daily-os-v16';
 
 // Files to pre-cache on install
 // self.location resolves relative paths correctly regardless of GitHub Pages subpath
